b40f9b1798479513f9409f354fb1c4a28dc118dc
fewspider <fewspider@gmail.com>
2019-04-24 17:29:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-function handleArgs(...args) {
-  let obj = args? args[0]: {};
-  let strs = _.clone(args);
+import * as _ from 'underscore';
+
+function handleRest(...rest) {
+  let obj = rest? rest[0]: {};
+  let strs = _.clone(rest);
   strs.shift();
 
   return {obj, strs};
@@ -19,35 +21,88 @@ function fetchLast(obj, strs) {
   return fetchLast(tempObj, tempStrs);
 }
 
-function fetchData(defaultValue, ...args) {
-  console.log(defaultValue);
+function fetchData(defaultValue, ...rest) {
   let rs = defaultValue;
   try {
-    let { obj, strs } = handleArgs(...args);
-    rs = fetchLast(obj, strs);
+    let { obj, strs } = handleRest(...rest);
+    let tempRs = fetchLast(obj, strs);
+    rs = tempRs? tempRs: defaultValue;
+
+    if (typeof rs !== typeof defaultValue) {
+      rs = defaultValue;
+    }
+
+    if (_.isArray(defaultValue) && !_.isArray(rs)) {
+      rs = defaultValue;
+    }
   } catch (e) {
+    console.log(e);
     rs = defaultValue;
   } finally {
     return rs;
   }
 }
 
-export function fetchObject(...args) {
-  return fetchData(defaultValue={}, ...args);
+export function fetchObject(...rest) {
+  return fetchData({}, ...rest);
 }
 
-export function fetchBoolean(...args) {
-  return fetchData(defaultValue=false, ...args);
+export function fetchBoolean(...rest) {
+  return fetchData(false, ...rest);
 }
 
-export function fetchString(...args) {
-  return fetchData(defaultValue='', ...args);
+export function fetchString(...rest) {
+  return fetchData('', ...rest);
 }
 
-export function fetchNumber(...args) {
-  return fetchData(defaultValue=0, ...args);
+export function fetchNumber(...rest) {
+  return fetchData(0, ...rest);
 }
 
-export function fetchArray(...args) {
-  return fetchData(defaultValue=[], ...args);
+export function fetchArray(...rest) {
+  return fetchData([], ...rest);
 }
+
+// let jsonData = {
+//   a: [
+//     {
+//       a1: '',
+//       a2: true,
+//       a3: false,
+//     },
+//     {
+//       a1: [1, 2, 3, 4],
+//       a2: true,
+//       a3: false,
+//     },
+//   ],
+//   b: {
+//     b1: 12,
+//     b2: 12.0,
+//     b3: 12.01,
+//     b4: 12.001
+//   },
+//   c: 0,
+//   d: null,
+//   e: '0'
+// };
+//
+// const a1 = fetchObject(jsonData, 'b');
+// console.log('fetchObject:');
+// console.log(a1);
+//
+// const a2 = fetchBoolean(jsonData, 'b');
+// console.log('fetchBoolean:');
+// console.log(a2);
+//
+// const a3 = fetchString(jsonData, 'b');
+// console.log('fetchString:');
+// console.log(a3);
+//
+// const a4 = fetchNumber(jsonData, 'b');
+// console.log('fetchNumber:');
+// console.log(a4);
+//
+// const a5 = fetchArray(jsonData, 'b');
+// console.log('fetchArray:');
+// console.log(a5);
--- test/index.spec.js
+++ test/index.spec.js
@@ -34,8 +34,64 @@ let jsonData = {
 describe('fetchObject', () => {
   describe('fetchObject function', () => {
     it('should be object!', () => {
-      const obj = fetchObject(jsonData, 'b');
-      assert.typeOf(obj, 'object');
-    })
-  })
-})
+      let test = fetchObject(jsonData, 'c');
+      assert.typeOf(test, 'object');
+    });
+
+    it('should be object!', () => {
+      let test = fetchObject(jsonData, 'd');
+      assert.typeOf(test, 'object');
+    });
+
+    it('should be equal!', () => {
+      let test = fetchObject(jsonData, 'd');
+      expect(test).to.eql({});
+    });
+  });
+});
+
+describe('fetchBoolean', () => {
+  describe('fetchBoolean function', () => {
+    it('should be boolean!', () => {
+      let obj = fetchBoolean(jsonData, 'b');
+      assert.typeOf(obj, 'boolean');
+    });
+
+    it('should be boolean!', () => {
+      let test = fetchBoolean(jsonData, 'd');
+      assert.typeOf(test, 'object');
+    });
+
+    it('should be equal!', () => {
+      let test = fetchBoolean(jsonData, 'd');
+      expect(test).to.eql(false);
+    });
+  });
+});
+
+describe('fetchString', () => {
+  describe('fetchString function', () => {
+    it('should be string!', () => {
+      let obj = fetchString(jsonData, 'b');
+      assert.typeOf(obj, 'string');
+    });
+  });
+});
+
+describe('fetchNumber', () => {
+  describe('fetchNumber function', () => {
+    it('should be number!', () => {
+      let obj = fetchNumber(jsonData, 'b');
+      assert.typeOf(obj, 'number');
+    });
+  });
+});
+
+describe('fetchArray', () => {
+  describe('fetchArray function', () => {
+    it('should be array!', () => {
+      let obj = fetchArray(jsonData, 'b');
+      assert.typeOf(obj, 'array');
+    });
+  });
+});
