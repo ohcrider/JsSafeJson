dcfdc0dd6e68a8638fe18e22fab2e8fb5be289ae
fewspider <fewspider@gmail.com>
2019-04-24 17:35:10 +0800

    update

--- test/index.spec.js
+++ test/index.spec.js
@@ -26,72 +26,91 @@ let jsonData = {
     b3: 12.01,
     b4: 12.001
   },
-  c: 0,
+  c: 233,
   d: null,
   e: '0'
 };
 
 describe('fetchObject', () => {
-  describe('fetchObject function', () => {
-    it('should be object!', () => {
-      let test = fetchObject(jsonData, 'c');
-      assert.typeOf(test, 'object');
-    });
+  it('should be object!', () => {
+    let test = fetchObject(jsonData, 'c');
+    assert.typeOf(test, 'object');
+  });
 
-    it('should be object!', () => {
-      let test = fetchObject(jsonData, 'd');
-      assert.typeOf(test, 'object');
-    });
+  it('should be object!', () => {
+    let test = fetchObject(jsonData, 'd');
+    assert.typeOf(test, 'object');
+  });
 
-    it('should be equal!', () => {
-      let test = fetchObject(jsonData, 'd');
-      expect(test).to.eql({});
-    });
+  it('should be equal!', () => {
+    let test = fetchObject(jsonData, 'd');
+    expect(test).to.eql({});
   });
 });
 
 describe('fetchBoolean', () => {
-  describe('fetchBoolean function', () => {
-    it('should be boolean!', () => {
-      let obj = fetchBoolean(jsonData, 'b');
-      assert.typeOf(obj, 'boolean');
-    });
+  it('should be boolean!', () => {
+    let obj = fetchBoolean(jsonData, 'b');
+    assert.typeOf(obj, 'boolean');
+  });
 
-    it('should be boolean!', () => {
-      let test = fetchBoolean(jsonData, 'd');
-      assert.typeOf(test, 'object');
-    });
+  it('should be boolean!', () => {
+    let test = fetchBoolean(jsonData, 'd');
+    assert.typeOf(test, 'boolean');
+  });
 
-    it('should be equal!', () => {
-      let test = fetchBoolean(jsonData, 'd');
-      expect(test).to.eql(false);
-    });
+  it('should be equal!', () => {
+    let test = fetchBoolean(jsonData, 'd');
+    expect(test).to.eql(false);
   });
 });
 
 describe('fetchString', () => {
-  describe('fetchString function', () => {
-    it('should be string!', () => {
-      let obj = fetchString(jsonData, 'b');
-      assert.typeOf(obj, 'string');
-    });
+  it('should be string!', () => {
+    let obj = fetchString(jsonData, 'b');
+    assert.typeOf(obj, 'string');
+  });
+
+  it('should be string!', () => {
+    let test = fetchString(jsonData, 'd');
+    assert.typeOf(test, 'string');
+  });
+
+  it('should be equal!', () => {
+    let test = fetchString(jsonData, 'd');
+    expect(test).to.eql('');
   });
 });
 
 describe('fetchNumber', () => {
-  describe('fetchNumber function', () => {
-    it('should be number!', () => {
-      let obj = fetchNumber(jsonData, 'b');
-      assert.typeOf(obj, 'number');
-    });
+  it('should be number!', () => {
+    let obj = fetchNumber(jsonData, 'b');
+    assert.typeOf(obj, 'number');
+  });
+  it('should be number!', () => {
+    let test = fetchNumber(jsonData, 'd');
+    assert.typeOf(test, 'number');
+  });
+
+  it('should be equal!', () => {
+    let test = fetchNumber(jsonData, 'd');
+    expect(test).to.eql(0);
   });
 });
 
 describe('fetchArray', () => {
-  describe('fetchArray function', () => {
-    it('should be array!', () => {
-      let obj = fetchArray(jsonData, 'b');
-      assert.typeOf(obj, 'array');
-    });
+  it('should be array!', () => {
+    let obj = fetchArray(jsonData, 'b');
+    assert.typeOf(obj, 'array');
+  });
+
+  it('should be array!', () => {
+    let test = fetchArray(jsonData, 'd');
+    assert.typeOf(test, 'array');
+  });
+
+  it('should be equal!', () => {
+    let test = fetchArray(jsonData, 'd');
+    expect(test).to.eql([]);
   });
 });
