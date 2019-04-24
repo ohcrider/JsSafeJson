f645775d66b223a25bce9775995815b02dd87233
fewspider <fewspider@gmail.com>
2019-04-24 15:48:38 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,3 +1,34 @@
+import {
+  fetchObject,
+  fetchBoolean,
+  fetchString,
+  fetchNumber,
+  fetchArray } from '../src/index';
+
+let jsonData = {
+  a: [
+    {
+      a1: '',
+      a2: true,
+      a3: false,
+    },
+    {
+      a1: [1, 2, 3, 4],
+      a2: true,
+      a3: false,
+    },
+  ],
+  b: {
+    b1: 12,
+    b2: 12.0,
+    b3: 12.01,
+    b4: 12.001
+  },
+  c: 0,
+  d: null,
+  e: '0'
+};
+
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
