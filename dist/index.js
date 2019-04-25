6802505cf574014fd06cde91461f7731096e34db
fewspider <fewspider@gmail.com>
2019-04-25 17:50:44 +0800

    update

---
+++ dist/index.js
@@ -0,0 +1,121 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fetchObject = fetchObject;
+exports.fetchBoolean = fetchBoolean;
+exports.fetchString = fetchString;
+exports.fetchNumber = fetchNumber;
+exports.fetchArray = fetchArray;
+
+var _ = _interopRequireWildcard(require("underscore"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function handleRest() {
+  for (var _len = arguments.length, rest = new Array(_len), _key = 0; _key < _len; _key++) {
+    rest[_key] = arguments[_key];
+  }
+
+  var obj = rest ? rest[0] : {};
+
+  var strs = _.clone(rest);
+
+  strs.shift();
+  return {
+    obj: obj,
+    strs: strs
+  };
+}
+
+function fetchLast(obj, strs) {
+  var tempStrs = _.clone(strs);
+
+  var tempStr = strs[0];
+  var tempObj = obj[tempStr];
+
+  if (tempStrs.length === 1) {
+    return tempObj;
+  }
+
+  tempStrs.shift();
+  return fetchLast(tempObj, tempStrs);
+}
+
+function fetchData(defaultValue) {
+  var rs = defaultValue;
+
+  try {
+    for (var _len2 = arguments.length, rest = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+      rest[_key2 - 1] = arguments[_key2];
+    }
+
+    var _handleRest = handleRest.apply(void 0, rest),
+        obj = _handleRest.obj,
+        strs = _handleRest.strs;
+
+    var tempRs = fetchLast(obj, strs);
+
+    if (_.isNumber(defaultValue) && _.isString(tempRs)) {
+      tempRs = Number(tempRs);
+    }
+
+    rs = tempRs ? tempRs : defaultValue;
+
+    if (_typeof(rs) !== _typeof(defaultValue)) {
+      rs = defaultValue;
+    }
+
+    if (_.isArray(defaultValue) && !_.isArray(rs)) {
+      rs = defaultValue;
+    }
+  } catch (e) {
+    // console.log(e);
+    rs = defaultValue;
+  } finally {
+    return rs;
+  }
+}
+
+function fetchObject() {
+  for (var _len3 = arguments.length, rest = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+    rest[_key3] = arguments[_key3];
+  }
+
+  return fetchData.apply(void 0, [{}].concat(rest));
+}
+
+function fetchBoolean() {
+  for (var _len4 = arguments.length, rest = new Array(_len4), _key4 = 0; _key4 < _len4; _key4++) {
+    rest[_key4] = arguments[_key4];
+  }
+
+  return fetchData.apply(void 0, [false].concat(rest));
+}
+
+function fetchString() {
+  for (var _len5 = arguments.length, rest = new Array(_len5), _key5 = 0; _key5 < _len5; _key5++) {
+    rest[_key5] = arguments[_key5];
+  }
+
+  return fetchData.apply(void 0, [''].concat(rest));
+}
+
+function fetchNumber() {
+  for (var _len6 = arguments.length, rest = new Array(_len6), _key6 = 0; _key6 < _len6; _key6++) {
+    rest[_key6] = arguments[_key6];
+  }
+
+  return fetchData.apply(void 0, [0].concat(rest));
+}
+
+function fetchArray() {
+  for (var _len7 = arguments.length, rest = new Array(_len7), _key7 = 0; _key7 < _len7; _key7++) {
+    rest[_key7] = arguments[_key7];
+  }
+
+  return fetchData.apply(void 0, [[]].concat(rest));
+}
